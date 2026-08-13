9310f8f4b09f278d56703ff53704fcd498a293c3
Dhanraj-SH <dhanrajsh445@gmail.com>
2026-08-13 14:51:06 +0530

    solved question num 89

---
+++ leetcode/qno89.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/gray-code/
+
+// class Solution {
+//     public List<Integer> grayCode(int n) {
+
+//         List<Integer> list = new ArrayList<>();
+//         int size = 1 << n;
+
+//         for (int i = 0; i < size; i++) {
+//             list.add(i ^ (i >> 1));
+//         }
+
+//         return list;
+//     }
+// }
