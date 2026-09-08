db22685ee8c17f2db7c86338db301f0e7f2dae11
Dhanraj-SH <dhanrajsh445@gmail.com>
2026-09-08 23:04:36 +0530

    Solved question realted to string

---
+++ hackerrank/Easy/Solution_16.java
@@ -0,0 +1,59 @@
+// import java.io.*;
+
+
+
+// class Result {
+
+//     /*
+//      * Complete the 'isAnagram' function below.
+//      *
+//      * The function is expected to return an INTEGER.
+//      * The function accepts following parameters:
+//      *  1. STRING s
+//      *  2. STRING t
+//      */
+
+//     public static int isAnagram(String s, String t) {
+//     // Write your code here
+//         int n = s.length();
+//         int m = t.length(); 
+        
+//         if(n != m) return 1;
+        
+//         int sArr[] = new int[26];
+//         int tArr[] = new int[26];
+        
+//         for(int i = 0; i<n; i++){
+//             sArr[s.charAt(i) - 'a']++;
+//         }
+        
+//         for(int i = 0; i<m; i++){
+//             tArr[t.charAt(i) - 'a']++;
+//         }
+        
+//         for(int i = 0; i<26; i++){
+//             if(sArr[i] != tArr[i]){
+//                 return 0;
+//             }
+//         }
+        
+//         return 1;
+//     }
+
+// }
+
+// public class Solution_16 {
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+//         String s = bufferedReader.readLine();
+
+//         String t = bufferedReader.readLine();
+
+//         int result = Result.isAnagram(s, t);
+
+//         System.out.println(result);
+
+//         bufferedReader.close();
+//     }
+// }
