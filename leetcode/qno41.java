823ec221498d49509b0b4a4bdd16a2e03fe94f7a
Dhanraj-SH <dhanrajsh445@gmail.com>
2026-09-11 11:32:32 +0530

    Solved question number 41

---
+++ leetcode/qno41.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/first-missing-positive/
+
+// class Solution {
+//     public int firstMissingPositive(int[] nums) {
+//         int n = nums.length;
+//         boolean seen[] = new boolean[n+1];
+
+//         for(int num: nums){
+//             if(num >0 && num <= n){
+//                 seen[num] = true;
+//             }
+//         }
+
+//         for(int i = 1; i < seen.length; i++){
+//             if(!seen[i]){
+//                 return i;
+//             }
+//         }
+
+//         return n + 1;
+//     }
+// }
